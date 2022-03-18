e4e39fa570a00c9969d28a422fc19e9316544619
MarkReddy87 <markreddy87@gmail.com>
2022-03-18 11:54:02 +0000

    Added adjust_bag and remove_from_bag views. Created bag_tools.py to handle the subtotal calculation

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """ A function to calculate the subtotal """
+    return price * quantity
