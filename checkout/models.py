c6c8c52f1f7bdd7dd1d7a909a6cb3a5eccfaf947
MarkReddy87 <markreddy87@gmail.com>
2022-03-21 12:05:22 +0000

    Finished checkout model functions

--- checkout/models.py
+++ checkout/models.py
@@ -45,12 +45,15 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
             super().save(*args, *kwargs)
 
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     """ models to track individual items within te order """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    qantity = models.IntegerField(null=False, blank=False, default=0)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
@@ -61,4 +64,7 @@ class OrderLineItem(models.Model):
 
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, *kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
 
