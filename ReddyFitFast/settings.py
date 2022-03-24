ef50b84919905e74b40a8165cb5975c35ae2c3f5
MarkReddy87 <markreddy87@gmail.com>
2022-03-24 15:24:18 +0000

    set up Procfile, changed allowed hosts and installed gunicorn to prep for deployment

--- ReddyFitFast/settings.py
+++ ReddyFitFast/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i=g!yab(ouv8r-bn(mxfn&c3*j8a!2k#xi$fl0&$l#gcwn#t&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reddy-fit-fast.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'ReddyFitFast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
