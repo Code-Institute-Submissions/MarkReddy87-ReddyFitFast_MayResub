9ddc4018eb63fbc1f74ff606bdc9bfd07bba40be
MarkReddy87 <markreddy87@gmail.com>
2022-03-29 15:16:56 +0000

    Added review admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """ Review Admin setup """
+    fields = ('title', 'body', 'image', 'posted_by',
+              'created_on', 'approved',)
+
+    list_filter = ('approved', 'created_on')
+
+    ordering = ('-created_on')
+
+    def approved_review(self, request, queryset):
+        """ approved function """
+        queryset.update(approved=True)
+
+
+admin.site.register(Review)
