36c572b5c0f7d93314adfbef9576f67374b18f82
MarkReddy87 <markreddy87@gmail.com>
2022-03-29 16:02:46 +0000

    Added review view, url and template file

--- review/views.py
+++ review/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from djange.contrib import messages
 
-# Create your views here.
+from .forms import ReviewForm
+
+def review(request):
+    """ review view """
+    review_form = ReviewForm()
+    template = 'review/review.html'
+    context = {
+        'review_form': review_form,
+    }
+
+    return redirect(request, template, context)
