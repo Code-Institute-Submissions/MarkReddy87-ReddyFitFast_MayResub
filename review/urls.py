8fe9146c3a73cf029af6e882e2c13859b731599b
MarkReddy87 <markreddy87@gmail.com>
2022-04-29 13:52:51 +0000

    Added content to edit_review.html, updated edit link in review_detail.html, added url path for edit_review and wrote edit_review view

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.review_list, name='review'),
     path('add/', views.add_review, name='add_review'),
     path('<int:review_id>/', views.review_detail, name='review_detail'),
+    path('edit/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
--- review/views.py
+++ review/views.py
@@ -16,6 +16,18 @@ def review_list(request):
     return render(request, 'review/reviews.html', context)
 
 
+def review_detail(request, review_id):
+    """ A view to show individual review details """
+
+    review = get_object_or_404(Review, pk=review_id)
+
+    context = {
+        'review': review,
+    }
+
+    return render(request, 'review/review_detail.html', context)
+
+
 def add_review(request):
     """ review view """
     if request.method == 'POST':
@@ -40,13 +52,25 @@ def add_review(request):
     return render(request, template, context)
 
 
-def review_detail(request, review_id):
-    """ A view to show individual review details """
-
+def edit_review(request, review_id):
+    """ Edit a review on the site """
     review = get_object_or_404(Review, pk=review_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated review!')
+            return redirect(reverse('review_detail', args=[review.id]))
+        else:
+            messages.error(request, 'Failed to update review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm(instance=review)
+        messages.info(request, f'You are editing {review.title}')
 
+    template = 'review/edit_review.html'
     context = {
+        'form': form,
         'review': review,
     }
 
-    return render(request, 'review/review_detail.html', context)
+    return render(request, template, context)
