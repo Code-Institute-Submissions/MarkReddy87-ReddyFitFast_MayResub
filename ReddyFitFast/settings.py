9afb47022fc87e4ca9cca2f8adb85f042455b0e3
MarkReddy87 <markreddy87@gmail.com>
2022-05-04 13:02:28 +0000

    removed django_extensions from settings, attempting heroku push again

--- ReddyFitFast/settings.py
+++ ReddyFitFast/settings.py
@@ -54,22 +54,7 @@ INSTALLED_APPS = [
     # Others
     'crispy_forms',
     'storages',
-    "django_extensions",
 ]
-
-GRAPH_MODELS = {
-  'all_applications': True,
-  'group_models': True,
-  "app_labels": [
-        "bag",
-        "checkout",
-        "home",
-        "products",
-        "profiles",
-        "review",
-        "wishlist",
-    ],
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
